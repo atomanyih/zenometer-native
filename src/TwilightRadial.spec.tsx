9e1f1131880e82a0d1f6e865605f79b1b3d909a3
August Toman-Yih <august.toman.yih@gmail.com>
2020-11-05 12:06:03 -0800

    stop twilight radial from complaining

--- src/TwilightRadial.spec.tsx
+++ src/TwilightRadial.spec.tsx
@@ -3,10 +3,8 @@ import {render} from '@testing-library/react-native';
 
 import TwilightRadial from './TwilightRadial';
 
-// describe('<TwilightRadial />', () => {
-//   it('renders a path', () => {
-//     const {getByTestId, debug} = render(<TwilightRadial/>);
-//     debug();
-//     getByTestId('arc');
-//   });
-// });
+describe('<TwilightRadial />', () => {
+  it('renders a path', () => {
+    const {getByTestId, debug} = render(<TwilightRadial/>);
+  });
+});
